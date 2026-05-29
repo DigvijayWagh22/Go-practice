ae40b0af49478d36ea8a2c9aa27478262ff1cd4f
Digvijay Subhash Wagh <digvijay@Digvijay>
2026-05-29 09:12:40 +0530

    Challenge 1

---
+++ ClassicChallenges/Challenge1/sumofTwoNumbers.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func Sum(a int, b int) int {
+	return a + b
+}
+
+func main() {
+	var a, b int
+
+	_, err := fmt.Scanf("%d, %d", &a, &b)
+	if err != nil {
+		fmt.Println("Error reading intput: ", err)
+	}
+
+	result := Sum(a, b)
+	fmt.Println(result)
+}
